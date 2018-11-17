47457eaaaf8f56e1123db08dffe70b49e32d0662
Mallory MOUSSON <mmousson@e3r9p20.42.fr>
2018-11-17 17:14:48 +0100

    correction on lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: guroux <guroux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 20:59:58 by guroux            #+#    #+#             */
-/*   Updated: 2018/11/16 21:18:37 by guroux           ###   ########.fr       */
+/*   Updated: 2018/11/17 17:14:29 by mmousson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 	if (content)
 	{
 		if (!(list->content = (void *)malloc(sizeof(void) * content_size + 1)))
+		{
+			free(list);
 			return (NULL);
+		}
 		ft_memcpy(list->content, content, content_size);
 		list->content_size = content_size;
 	}
